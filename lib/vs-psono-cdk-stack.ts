1fe8b60cd4606a502d5cd74fef3c12dcbd3a4dee
Valentin <valentin.savenko@signavio.com>
2021-09-25 20:51:11 +0200

    switch to ec2 and serverless postgres

--- lib/vs-psono-cdk-stack.ts
+++ lib/vs-psono-cdk-stack.ts
@@ -1,46 +1,121 @@
-import * as cdk from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
-import * as ecs from "@aws-cdk/aws-ecs";
-import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
-
-import { SesSmtpCredentials } from 'ses-smtp-credentials-cdk';
+import * as iam from '@aws-cdk/aws-iam';
+import * as rds from "@aws-cdk/aws-rds";
+import { Asset } from '@aws-cdk/aws-s3-assets';
+import * as cdk from '@aws-cdk/core';
+import { CfnOutput, Duration } from "@aws-cdk/core";
+import { KeyPair } from 'cdk-ec2-key-pair';
+import * as path from 'path';
 
 export class VsPsonoCdkStack extends cdk.Stack {
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a Key Pair to be used with this EC2 Instance
+    const key = new KeyPair(this, 'KeyPair', {
+      name: 'cdk-keypair',
+      description: 'Key Pair created with CDK Deployment',
+    });
+    key.grantReadOnPublicKey
+
+    // Create new VPC with 2 Subnets
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      natGateways: 0,
+      cidr: "10.33.0.0/16",
+      subnetConfiguration: [{
+        name: "public",
+        subnetType: ec2.SubnetType.PUBLIC
+      },{
+        name: "isolated",
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+      }]
+    });
+
+    // Allow SSH (TCP Port 22) access from anywhere
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow SSH (TCP port 22) in',
+      allowAllOutbound: true
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
+
+    const role = new iam.Role(this, 'ec2Role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    })
+
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
+
+    // Use Latest Amazon Linux Image - CPU Type ARM64
+    const ami = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.ARM_64
+    });
+
+    // Create the instance using the Security Group, AMI, and KeyPair defined in the VPC created
+    const ec2Instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
+      machineImage: ami,
+      securityGroup: securityGroup,
+      keyName: key.keyPairName,
+      role: role,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+    });
+
+    // Create an asset that will be used as part of User Data to run on first load
+    const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../src/config.sh') });
+    const localPath = ec2Instance.userData.addS3DownloadCommand({
+      bucket: asset.bucket,
+      bucketKey: asset.s3ObjectKey,
+    });
+
+    ec2Instance.userData.addExecuteFileCommand({
+      filePath: localPath,
+      arguments: '--verbose -y'
+    });
+    asset.grantRead(ec2Instance.role);
+
+    // Create outputs for connecting
+    new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
+    new cdk.CfnOutput(this, 'Key Name', { value: key.keyPairName })
+    new cdk.CfnOutput(this, 'Download Key Command', { value: 'aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem' })
+    new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp })
+
+// --------------------- DB --------------------- 
+    // let db = new rds.DatabaseInstance(this, 'InstanceWithUsername', {
+    //   engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_13_3 }),
+    //   instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MICRO),
+    //   allocatedStorage: 5,
+    //   deleteAutomatedBackups: true,
+    //   backupRetention: cdk.Duration.days(0),
+    //   vpc: vpc,
+    //   credentials: rds.Credentials.fromGeneratedSecret('postgres'), // Creates an admin user of postgres with a generated password
+    // });
+    const postgresCluster = new rds.ServerlessCluster(this, 'psonoPostgres', {
+      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+      parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql10'),
+      vpc,
+      scaling: {
+        autoPause: Duration.minutes(10), // default is to pause after 5 minutes of idle time
+        minCapacity: rds.AuroraCapacityUnit.ACU_8, // default is 2 Aurora capacity units (ACUs)
+        maxCapacity: rds.AuroraCapacityUnit.ACU_32, // default is 16 Aurora capacity units (ACUs)
+      },
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'), // Creates an admin user of postgres with a generated password
+      
+    });
+    
+    new CfnOutput(this, "postgres-Endpoint",{
+      value: postgresCluster.clusterEndpoint.hostname
+    })
+
+    postgresCluster.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432))
   }
-
- smtpCredentials = new SesSmtpCredentials(this, 'Credentials', {
-    region: 'eu-central-1'
-});
-// new ssm.StringParameter(this, 'CredentialsParameter', {
-//     parameterName: 'email',
-//     stringValue: JSON.stringify({
-//         username: smtpCredentials.username(),
-//         password: smtpCredentials.password(),
-//     })
-// });
-
-  vpc = new ec2.Vpc(this, "MyVpc", {
-    maxAzs: 1 // Default is all AZs in region
-  });
-
-  cluster = new ecs.Cluster(this, "MyCluster", {
-    vpc: this.vpc
-  });
-
-// Create a load-balanced Fargate service and make it public
-  ecs_stuff = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-    cluster: this.cluster, // Required
-    cpu: 512, // Default is 256
-    desiredCount: 1, // Default is 1
-    taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-    memoryLimitMiB: 512, // Default is 512
-    publicLoadBalancer: true // Default is false
-  });
-
 
 
 }
